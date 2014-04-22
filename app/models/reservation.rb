587ff214519ad1844b7988cf1b7b92177cb4e98e
Joshua Rivers <joshua.rivers@gmail.com>
2014-04-22 14:43:44 -0400

    Validates fields for reservations and added date field to reservations table

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,2 +1,7 @@
 class Reservation < ActiveRecord::Base
+  validates :name, presence: true
+  validates :phone_number, presence: true
+  validates :time, presence: true
+  validates :date, presence: true
+
 end
