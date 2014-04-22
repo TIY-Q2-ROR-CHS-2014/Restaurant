54d688d1e921969d5d3207aaba9c568403420adf
Joshua Rivers <joshua.rivers@gmail.com>
2014-04-22 16:00:53 -0400

    Added resources, workflow_states, validaiton

--- app/models/table.rb
+++ app/models/table.rb
@@ -1,2 +1,17 @@
 class Table < ActiveRecord::Base
+  validates :table_number, presence: true
+  validates :seats, presence: true
+  validates :type, presence: true
+
+  include Workflow
+  workflow do
+    state :available do
+      event :reserved, transitions_to: :reserved
+    end
+    state :reserved do
+      event :available, transitions_to: :available
+    end
+    
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140422184141) do
+ActiveRecord::Schema.define(version: 20140422195106) do
 
   create_table "reservations", force: true do |t|
     t.string   "name"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20140422184141) do
     t.string   "type"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "workflow_state"
   end
 
 end
