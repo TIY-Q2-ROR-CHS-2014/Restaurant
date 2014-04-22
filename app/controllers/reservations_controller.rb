c7a428d23434b9d37ec515662b40d3882e664594
Chisolm Wilson <cw@commonesystems.com>
2014-04-22 15:44:47 -0400

    mods to controllers

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,21 +1,38 @@
 class ReservationsController < ApplicationController
 
 	def new
+		@reservation = Reservation.new
 	end
 
 	def create
+		@reservation = Reservation.new reservation_params
+		if @reservation.save
+			flash[:notice]= "Reservation added OK."
+			redirect_to root_path
+		else
+		    flash[:error]= "Error adding Reservation!"
+		    render :new
+		end
 	end
 
 	def edit
+		@reservation = Reservation.find params[:id]
 	end
 
 	def update
+		@reservation = Reservation.find params[:id]
+		@reservation.update_attributes reservation_params
+		redirect_to root_path
 	end
 
 	def destroy
+		@reservation = Reservation.find params[:id]
+		@reservation.delete
+		redirect_to root_path
 	end
 
 	def show
+		@reservation = Reservation.find params[:id]
 	end
 
 
